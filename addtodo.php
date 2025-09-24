b39d7994b6b1b214a185bdf39aa4f33321c1c0e6
Matthijs Hulshof <hulshofmatthijs@gmail.com>
2025-09-24 11:24:46 +0200

    Update task insertion to add only non-empty tasks to the database

--- addtodo.php
+++ addtodo.php
@@ -2,11 +2,13 @@
 include "database.php";
 
 // Checks if addtask is set
-// Inserts task into database
+// Inserts task into database if not empty
 // Return to index.php
 if(isset($_POST["addtask"])){
-    $task = $_POST["task"];
-    $conn -> query("INSERT INTO tasks (task) VALUES ('$task')");
+    $task = trim($_POST["task"]);
+    if(!empty($task)){
+        $conn->query("INSERT INTO tasks (task) VALUES ('$task')");
+    }
     header("Location: index.php");
 }
 
